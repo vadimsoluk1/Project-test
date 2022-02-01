5ceb47483193ffdc267b10cdc2de4a59a5afdc68
Dmytro <=>
2022-02-01 13:58:47 +0200

    added constructor end some change

--- Project test/Source.cpp
+++ Project test/Source.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+using namespace std;
+
 
 class point {
-	int x, y;
+	int x, y , z;
+public:
+	point() { x = y = z = 0; }
+
+	void print() {
+		cout << x << y << z;
+	}
 };
 
 int main() {
